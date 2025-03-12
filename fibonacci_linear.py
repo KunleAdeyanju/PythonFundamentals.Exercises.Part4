d8eb3bfe45dcaf77119c565d9d846eb5f4914fdd
Kunle <kunle@Zipcoders-MacBook-Pro-5.local>
2025-03-12 15:23:03 -0400

    now fibonacci_recursive is working

--- fibonacci_linear.py
+++ fibonacci_linear.py
@@ -3,6 +3,6 @@ sequence = [0,1]
 for i in range(2,31,1):
     sequence.append(sequence[i-1]+sequence[i-2])
 
-x = int (input('Enter a number between 0 an 30\n'))
+x = int (input('Enter a number between 0 an 30:\n'))
 
 print("The " + str(x) + "'th fibonacci number is " + str(sequence[x]) )
